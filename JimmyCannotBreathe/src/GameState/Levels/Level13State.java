f1f55a03d63842ff31df4f418918adff950ffcdc
NINJAMUSICIAN <j80mar4@gmail.com>
2013-10-03 17:08:01 -0600

    Added Coins to 13 and gave jimmy new spritesheet
    Jimmy is now red and blue :D and Level 13 is pretty much done. Now we
    begin design of 14 and minimalist spritesheets for all enemies.

--- JimmyCannotBreathe/src/GameState/Levels/Level13State.java
+++ JimmyCannotBreathe/src/GameState/Levels/Level13State.java
@@ -1,15 +1,17 @@
 package GameState.Levels;
 
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
+import Entity.BronzeCoin;
 import Entity.Coin;
 import Entity.Enemy;
 import Entity.HUD;
 import Entity.Player;
+import Entity.SilverCoin;
 import Entity.Tombstone;
-import Entity.Enemies.Devil;
 import GameState.GameState;
 import GameState.GameStateManager;
 import Main.GamePanel;
@@ -23,13 +25,11 @@ public class Level13State extends GameState {
 	
 	private Player player;
 	private ArrayList<Enemy> enemies;
+	private ArrayList<Coin> coins;
 	
 	private HUD hud;
-	
-	private Devil devil;
-	private boolean triggered = false;
-	
-	private Tombstone tomby;
+		
+	private Tombstone finish;
 	
 	public Level13State(GameStateManager gsm){
 		this.gsm = gsm;
@@ -47,21 +47,115 @@ public class Level13State extends GameState {
 		
 		bg = new Background("/Backgrounds/White.png", 0.1);
 		
-		player = new Player("/Sprites/RedJimmy.png", tileMap);
+		player = new Player("/Sprites/RedBlueJimmy.png", tileMap);
 		player.setPosition(120, 120);
+		finish = new Tombstone(2955, 945, GameStateManager.MENUSTATE, gsm, tileMap, false);
+//2955, 945
 		
 		hud = new HUD(player);
+		
+		populateCoins();
 	}
 
-	@Override
-
+	public void populateCoins(){
+		coins = new ArrayList<Coin>();
+		
+		BronzeCoin bc;
+		SilverCoin sc;
+		
+		Point[] bronzePoints = new Point[]{ 
+				new Point(585, 855),
+				new Point(675, 855),
+				new Point(1005, 945),
+				new Point(1035, 945),
+				new Point(1125, 945),
+				new Point(1155, 945),
+				new Point(1065, 795),
+				new Point(1095, 795),
+				new Point(1035, 705),
+				new Point(1125, 705),
+				new Point(975, 615),
+				new Point(855, 555),
+				new Point(1035, 525),
+				new Point(1125, 525),
+				new Point(1275, 555),
+				new Point(1335, 555),
+				new Point(915, 435),
+				new Point(975, 435),
+				new Point(1185, 435),
+				new Point(1245, 435),
+				new Point(1155, 345),
+				new Point(1005, 345),
+				new Point(1455, 915),
+				new Point(1515, 855),
+				new Point(1575, 795),
+				new Point(1635, 735),
+				new Point(1725, 645),
+				new Point(1665, 465),
+				new Point(1725, 465),
+				new Point(1785, 465),
+				new Point(1695, 315),
+				new Point(1725, 315),
+				new Point(1755, 315),
+				new Point(1815, 735),
+				new Point(1875, 795),
+				new Point(1935, 855),
+				new Point(1995, 915),
+				new Point(2355, 855),
+				new Point(2385, 885),
+				new Point(2415, 915),
+				new Point(2625, 915),
+				new Point(2745, 795),
+				new Point(2865, 675),
+				};
+		
+		for(int i = 0; i < bronzePoints.length;i++){
+			bc = new BronzeCoin(tileMap);
+			bc.setPosition(bronzePoints[i].x, bronzePoints[i].y);
+			coins.add(bc);
+		}
+		
+		Point[] silverPoints = new Point[]{ 
+				new Point(1065, 945),
+				new Point(1095, 945),
+				new Point(1035, 795),
+				new Point(1125, 795),
+				new Point(1185, 615),
+				new Point(825, 555),
+				new Point(885, 555),
+				new Point(1305, 555),
+				new Point(1065, 495),
+				new Point(1095, 495),
+				new Point(945, 435),
+				new Point(1215, 435),
+				new Point(1065, 255),
+				new Point(1095, 255),
+				new Point(1635, 555),
+				new Point(1815, 555),
+				new Point(1605, 375),
+				new Point(1845, 375),
+				new Point(1575, 255),
+				new Point(1875, 255),
+				new Point(2685, 855),
+				new Point(2805, 735),
+				new Point(2925, 615),
+				};
+		
+		for(int i = 0; i < silverPoints.length;i++){
+			sc = new SilverCoin(tileMap);
+			sc.setPosition(silverPoints[i].x, silverPoints[i].y);
+			coins.add(sc);
+		}
+		
+	}
+	
 	public void update() {
 		player.update();
 	//	player.checkAttack(enemies);
-//		player.checkCoins(coins);
+		player.checkCoins(coins);
 //		
-//		finish.update();
-//		finish.checkGrab(player);
+		finish.update();
+		finish.checkGrab(player);
 //		
 		bg.setPosition(tileMap.getx(), 0);
 		
@@ -90,15 +184,15 @@ public class Level13State extends GameState {
 //			}
 //		}
 //		
-//		for(int i = 0; i < coins.size(); i++){
-//			Coin c = coins.get(i);
-//			c.update();
-//			if(c.shouldRemove()){
-//				coins.remove(i);
-//				c.addScore();
-//				i--;
-//			}
-//		}
+		for(int i = 0; i < coins.size(); i++){
+			Coin c = coins.get(i);
+			c.update();
+			if(c.shouldRemove()){
+				coins.remove(i);
+				c.addScore();
+				i--;
+			}
+		}
 		
 	}
 
@@ -106,11 +200,11 @@ public class Level13State extends GameState {
 		bg.draw(g);
 		tileMap.draw(g);
 		player.draw(g);
-//		finish.draw(g);
+		finish.draw(g);
 //		
-//		for(int i = 0; i < coins.size(); i++){
-//			coins.get(i).draw(g);
-//		}
+		for(int i = 0; i < coins.size(); i++){
+			coins.get(i).draw(g);
+		}
 		
 		for(int i = 0; i < enemies.size(); i++){
 			enemies.get(i).draw(g);
